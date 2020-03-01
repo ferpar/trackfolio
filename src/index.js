8ea3306ab5febf58f6cfb439af04818abd6a89de
fer <ferp@protonmail.ch>
2020-03-01 10:48:27 +0100

    wired callback mechanism into main index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import makeCallback from './express-callback'
 
 dotenv.config()
 
 const app = express()
 
 app.use(bodyParser.json())
+
+
+app.use(makeCallback(notFound))
 
 app.listen(process.env.PORT, () => {
   console.log("Server is listening on port " + process.env.PORT)
