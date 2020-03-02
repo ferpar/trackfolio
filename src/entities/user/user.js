67fb0bf67be7e605d05ed8110abd9bc244458341
fer <ferp@protonmail.ch>
2020-03-02 18:13:08 +0100

    corrected id generation, added error conditions for User entity

--- src/entities/user/user.js
+++ src/entities/user/user.js
@@ -1,11 +1,23 @@
 export default function buildMakeUser ({ Id, md5 }) {
   return function makeUser ({
     createdOn = Date.now(),
-    id,
+    id = Id.makeId(),
     modifiedOn = Date.now(),
     password,
     username
   } = {}) {
+    if (!Id.isValidId(id)) {
+      throw new Error('User must have a valid id')
+    }
+    if (!username) {
+      throw new Error('User must have a username')
+    }
+    if (username.length < 2) {
+      throw new Error('username must be at least 2 characters long')
+    }
+    if (!password) {
+      throw new Error('User must have a password')
+    }
     return Object.freeze({
       getCreatedOn: () => createdOn,
       getId: () => id,
