8fbf2ecb4dbce69f71c3d64ed0f90d1e3f76fe97
fer <ferp@protonmail.ch>
2020-03-03 23:06:02 +0100

    further correctionsto the post-user controller output

--- src/controllers/post-user.js
+++ src/controllers/post-user.js
@@ -16,7 +16,7 @@ export default function makePostUSer ({signUpUser}) {
           'Last-Modified': new Date(posted.modifiedOn).toUTCString()
         },
         statusCode: 201,
-        body: { ...posted }
+        body: { ...posted._doc }
       }
     } catch (err) {
       console.error("[Controller] Error on user signup", err)
